7a6168001aee8c95f83020135d827996c87d9d15
Zakharov Alexey <alexey.zakharov@altoros.com>
2017-02-07 12:39:39 +0300

    Added API version check

--- web.py
+++ web.py
@@ -1,9 +1,14 @@
 import os
 from flask import Flask, jsonify, make_response, request
+from functools import wraps
 
 app = Flask(__name__)
 
 log = app.logger
+
+X_BROKER_API_MAJOR_VERSION = 2
+X_BROKER_API_MINOR_VERSION = 10
+X_BROKER_API_VERSION_NAME = 'X-Broker-Api-Version'
 
 # Service plans
 plan_one = {
@@ -24,8 +29,35 @@ my_service = {
 
 my_services = {"services": [my_service]}
 
+def api_version_is_valid(api_version):
+    version_data = api_version.split('.')
+    result = True
+    if (float(version_data[0]) < X_BROKER_API_MAJOR_VERSION or
+       (float(version_data[0]) == X_BROKER_API_MAJOR_VERSION and
+       float(version_data[1]) < X_BROKER_API_MINOR_VERSION)):
+                result = False
+    return result
+
+
+def requires_api_version(f):
+    @wraps(f)
+    def decorated(*args, **kwargs):
+        api_version = request.headers.get('X-Broker-Api-Version')
+        if (not api_version or not (api_version_is_valid(api_version))):
+            abort(412)
+        return f(*args, **kwargs)
+    return decorated
+
+
+@app.errorhandler(412)
+def version_mismatch(error):
+    return 'Version mismatch. Expected: {}: {}.{}'.format(
+        X_BROKER_API_VERSION_NAME,
+        X_BROKER_API_MAJOR_VERSION,
+        X_BROKER_API_MINOR_VERSION), 412
 
 @app.route('/v2/catalog')
+@requires_api_version
 def catalog():
     return jsonify(my_services)
 
